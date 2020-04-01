37e7dddc0f3d271259f546ba06b0fe8e6e77f16c
Abdul Rasheed <ebdulrasheed@gmail.com>
2020-04-02 00:41:31 +0500

    [LAB]: 5.2 Add Child Pages functionality

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
  
- function loadHeader () {
+function loadHeader () {
     wp_enqueue_script("slide_scripts", get_theme_file_uri("/js/scripts-bundled.js"), NULL, microtime(), true);
     wp_enqueue_style('cstm_fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
@@ -13,4 +13,3 @@ function features() {
 
 add_action('wp_enqueue_scripts', 'loadHeader');
 add_action('after_setup_theme', 'features');
-?>
